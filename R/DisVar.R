7c5b901458a03bee2d202b31e1d9ae8088beb5e9
Khunanon-Chanasongkhrams <6210210016@psu.ac.th>
2023-07-02 22:32:52 +0700

    Update output format + Gene column + Variant_type column

--- R/DisVar.R
+++ R/DisVar.R
@@ -59,7 +59,9 @@ DisVar <- function(file = "vcf_file_name.vcf"){
                         GWASdb_GRCh38.Ref,
                         GWASdb_GRCh38.Alt,
                         GWASdb_GRCh38.P_value,
-                        GWASdb_GRCh38.Gwas_trait
+                        GWASdb_GRCh38.Gwas_trait,
+                        GWASdb_GRCh38.Gene,
+                        GWASdb_GRCh38.Variant_type
                         FROM GWASdb_GRCh38, variant_data
                         WHERE variant_data.V2 = GWASdb_GRCh38.Position AND variant_data.V1 = GWASdb_GRCh38.Chr AND GWASdb_GRCh38.P_value < 0.0000001
                         ")
@@ -70,8 +72,6 @@ DisVar <- function(file = "vcf_file_name.vcf"){
   }
 
   #find variant that in the GRASP database
-  GRASP_GRCh38$Ref = NA
-  GRASP_GRCh38$Alt = NA
   op_df_GRASP <- sqldf("
                         SELECT
                         GRASP_GRCh38.Rsid,
@@ -82,19 +82,18 @@ DisVar <- function(file = "vcf_file_name.vcf"){
                         GRASP_GRCh38.Ref,
                         GRASP_GRCh38.Alt,
                         GRASP_GRCh38.P_value,
-                        GRASP_GRCh38.Phenotype
+                        GRASP_GRCh38.Gwas_trait,
+                        GRASP_GRCh38.Gene,
+                        GRASP_GRCh38.Variant_type
                         FROM GRASP_GRCh38, variant_data
                         WHERE variant_data.V2 = GRASP_GRCh38.Position AND variant_data.V1 = GRASP_GRCh38.Chr AND GRASP_GRCh38.P_value < 0.0000001
                        ")
-  colnames(op_df_GRASP)[9] <- "Gwas_trait"
   if (nrow(op_df_GRASP) > 0)
   {
     op_df_GRASP$DB <- "GRASP"
   }
 
   #find variant that in the GWAS catalog database
-  GWAS_catalog_GRCh38$Ref = NA
-  GWAS_catalog_GRCh38$Alt = NA
   op_df_GWAS_catalog <- sqldf("
                         SELECT
                         GWAS_catalog_GRCh38.Rsid,
@@ -105,7 +104,9 @@ DisVar <- function(file = "vcf_file_name.vcf"){
                         GWAS_catalog_GRCh38.Ref,
                         GWAS_catalog_GRCh38.Alt,
                         GWAS_catalog_GRCh38.P_value,
-                        GWAS_catalog_GRCh38.Gwas_trait
+                        GWAS_catalog_GRCh38.Gwas_trait,
+                        GWAS_catalog_GRCh38.Gene,
+                        GWAS_catalog_GRCh38.Variant_type
                         FROM GWAS_catalog_GRCh38, variant_data
                         WHERE variant_data.V2 = GWAS_catalog_GRCh38.Position AND variant_data.V1 = GWAS_catalog_GRCh38.Chr AND GWAS_catalog_GRCh38.P_value < 0.0000001
                         ")
@@ -116,9 +117,6 @@ DisVar <- function(file = "vcf_file_name.vcf"){
   }
 
   #find variant that in the GAD database
-  GAD_GRCh38$Ref = NA
-  GAD_GRCh38$Alt = NA
-  GAD_GRCh38$P_value = NA
   op_df_GAD <- sqldf("
                       SELECT
                       GAD_GRCh38.Rsid,
@@ -129,7 +127,9 @@ DisVar <- function(file = "vcf_file_name.vcf"){
                       GAD_GRCh38.Ref,
                       GAD_GRCh38.Alt,
                       GAD_GRCh38.P_value,
-                      GAD_GRCh38.Gwas_trait
+                      GAD_GRCh38.Gwas_trait,
+                      GAD_GRCh38.Gene,
+                      GAD_GRCh38.Variant_type
                       FROM GAD_GRCh38, variant_data
                       WHERE variant_data.V2 = GAD_GRCh38.Position AND variant_data.V1 = GAD_GRCh38.Chr")
 
@@ -139,8 +139,6 @@ DisVar <- function(file = "vcf_file_name.vcf"){
   }
 
   #find variant that in the Johnson and O'Donnell database
-  JnO_GRCh38$Ref = NA
-  JnO_GRCh38$Alt = NA
   op_df_JnO <- sqldf("
                       SELECT
                       JnO_GRCh38.Rsid,
@@ -151,7 +149,9 @@ DisVar <- function(file = "vcf_file_name.vcf"){
                       JnO_GRCh38.Ref,
                       JnO_GRCh38.Alt,
                       JnO_GRCh38.P_value,
-                      JnO_GRCh38.Gwas_trait
+                      JnO_GRCh38.Gwas_trait,
+                      JnO_GRCh38.Gene,
+                      JnO_GRCh38.Variant_type
                       FROM JnO_GRCh38, variant_data
                       WHERE variant_data.V2 = JnO_GRCh38.Position AND variant_data.V1 = JnO_GRCh38.Chr AND JnO_GRCh38.P_value < 0.0000001
                         ")
@@ -173,13 +173,15 @@ DisVar <- function(file = "vcf_file_name.vcf"){
   }
 
   #create results in a table
-  align_df <- setNames(data.frame(matrix(ncol = 8, nrow = nrow(op_df))), c("Disease", "Chr", "Position", "Variant_id", "Allele sample", "Allele DB", "Confident", "DB"))
+  align_df <- setNames(data.frame(matrix(ncol = 10, nrow = nrow(op_df))), c("Disease", "Chr", "Position", "Gene", "Variant_id", "Variant_type", "Allele sample", "Allele DB", "Confident", "DB"))
 
 
   align_df["Disease"] <- sqldf('SELECT Gwas_trait FROM op_df')
   align_df["Chr"] <- sqldf('SELECT Chr FROM op_df')
   align_df["Position"] <- sqldf('SELECT Position FROM op_df')
+  align_df["Gene"] <- sqldf('SELECT Gene FROM op_df')
   align_df["Variant_id"] <- sqldf('SELECT Rsid FROM op_df')
+  align_df["Variant_type"] <- sqldf('SELECT Variant_type FROM op_df')
 
   for (i in 1:nrow(op_df))
   {
